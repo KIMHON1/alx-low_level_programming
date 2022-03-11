9104e28900d36af7f46e67c05e8c543ee0f03013
KIMHON1 <honore.kimenyi@gmail.com>
2022-03-11 10:34:42 -0800

    collections 4 th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - entry points
+ *
+ * Return: 0 after printing the function
+ * /
+ int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle\n");
-return(0);
+return (0);
 }
