013e1ea30edc2fb8776732c710cd41796d6e66c8
KIMHON1 <honore.kimenyi@gmail.com>
2022-03-13 03:33:48 -0700

    6th task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - Entrypoint
-*
-*Return: always 0(success)
-*/
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("Size of a Char: %d byte(s)\n", sizeof(char));
