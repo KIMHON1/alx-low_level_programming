0ed750f978e2b579e76f9d47a3b1b30bfea8c0c2
KIMHON1 <honore.kimenyi@gmail.com>
2022-03-19 03:00:18 -0700

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,19 +7,18 @@
  * main - Entry point
  *
  * Return: return0 return void
- */
+ **/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char hola[] = "_putchar";
+	int c;
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(hola[c]);
+	}
 	_putchar('\n');
+
 
 	return (0);
 }
