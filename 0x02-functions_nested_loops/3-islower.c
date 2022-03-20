f9db481e5666f885a70911613b68f2e0bf2ec611
KIMHON1 <honore.kimenyi@gmail.com>
2022-03-20 08:54:23 -0700

    3rd task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_is lower  -returns 1 if lowercase alphabet
+ *and 0 other wis
+ *Return : 0 for uppercase, 1 for lowercas
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1)
+	}
+
+	els
+	{
+		retrun (0)
+	}
+}
+
