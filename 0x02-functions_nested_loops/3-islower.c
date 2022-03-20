0052ddfc56cf2dd5a0087fd887f7b86237df23b4
KIMHON1 <honore.kimenyi@gmail.com>
2022-03-20 09:20:51 -0700

    3rd task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include "main"
 /**
- *_islower - retruns 1 if lower case alphabet
- *0 other wise
- *@c: takes in a character
- *Retrun: 0 for upper case , 1 for lowercase
+ * _islower -returns 1 if lower case alphabet and 0 other wise
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
  */
+ 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
