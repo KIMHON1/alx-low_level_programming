403a45b20609caf7e5af0216a7ae75b305ba9917
KIMHON1 <honore.kimenyi@gmail.com>
2022-03-21 14:42:26 -0700

    5th task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,10 @@
-
 #include "main.h"
 
 /**
- *  * rev_string - reverse a string.
- *   *
- *    *@s: the string to be reversed.
- *     */
+ * rev_string - reverse a string.
+ *
+ *@s: the string to be reversed.
+ */
 
 void rev_string(char *s)
 {
@@ -20,13 +19,12 @@ void rev_string(char *s)
 	i = 0;
 
 	while (j > i)
-	{	
+	{
 		v1 = s[i];
 		v2 = s[j];
 		s[i] = v2;
-																				s[j] = v1;
+		s[j] = v1;
 		j--;
 		i++;
-		
 	}
 }
